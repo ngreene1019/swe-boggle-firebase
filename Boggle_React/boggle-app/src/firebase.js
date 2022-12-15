426a92eeed031d76bb5c251ea57b69cccfe917c3
Najah Greene <najah.greene@bison.howard.edu>
2022-12-15 20:32:47 +0000

    Add all files

---
+++ Boggle_React/boggle-app/src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCB40-_xomJ5gCKACYV-wp6oz4JXgm4_8M",
+  authDomain: "boggle-app-b8600.firebaseapp.com",
+  projectId: "boggle-app-b8600",
+  storageBucket: "boggle-app-b8600.appspot.com",
+  messagingSenderId: "903609298298",
+  appId: "1:903609298298:web:9ac76803c1cf65330d3ac3",
+  measurementId: "G-5L6K56X3K3"
+};
+
+
+export default firebase;
